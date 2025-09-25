dea7e1be86094563de902fb6d92232d850156ec5
practicas-y-cores-del-bootcamp <yasmin.azcona154@gmail.com>
2025-09-25 16:13:58 -0300

    se agrego el header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Main from './componentes/Main.jsx'
+import Header from './componentes/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
       <Main/>
      
     </>
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,6 @@
+import styles from './../css/Header.module.css'
+
+const Header =()=> { return (
+    <header className={styles.header}></header>
+);};
+export default Header
